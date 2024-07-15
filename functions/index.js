4f6387b7afd4d60a39beed2d40e3e527ac6d9a80
Ondřej Rejnek <ondrej.rejnek@gmail.com>
2024-07-15 17:17:58 +0200

    netlify deployment

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import request from "request";
+import serverless from "serverless-http";
 
 const app = express();
 const API_URL =
@@ -18,3 +19,5 @@ app.get("/api", (_req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+export default serverless(app);
